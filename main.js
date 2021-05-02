eda3bd1977de15b272aa12e1ad677ecfa0849ac9
unknown <anhnhattran97@gmail.com>
2021-05-02 16:32:27 +0700

    Version Branch_dev_A

--- main.js
+++ main.js
@@ -5,3 +5,9 @@ console.log("Chức năng thêm sinh viên");
 function del() {
     console.log("dev");
 }
+function add() {
+    console.log("add");
+}
+function devA() {
+    console.log("dev_A code chức năng tạo branch");
+}
